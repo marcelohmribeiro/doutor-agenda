be960f714e20e6a82ad6296874de4de7f8f66818
marcelohmribeiro <marcelomarquesribeiro1@gmail.com>
2025-06-03 22:29:35 -0300

    feat: implement authentication session handling and add dashboard pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,24 @@
-function DashboardPage() {
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import SignOutButton from "@/app/dashboard/components/sign-out-button";
+import { auth } from "@/lib/auth";
+
+async function DashboardPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/authentication");
+  }
+
   return (
     <div>
       <h1>Dashboard</h1>
+      <h1>{session?.user?.name}</h1>
+      <h1>{session?.user?.email}</h1>
+      <SignOutButton />
     </div>
   );
 }
