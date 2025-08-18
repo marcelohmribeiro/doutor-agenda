7b67491bc8a19ac275396a6f9091bd2ea3149ba1
marcelohmribeiro <marcelomarquesribeiro1@gmail.com>
2025-08-18 16:22:03 -0300

    fix: update middleware to exclude authentication route from public access

--- src/middleware.ts
+++ src/middleware.ts
@@ -18,6 +18,6 @@ export const config = {
     "/doctors",
     "/appointments",
     "/subscription",
-    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|authentication).*)",
   ],
 };
